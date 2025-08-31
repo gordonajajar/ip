fa18008bb5bcaf45a9f1f166fa4116a1ac1b5b9d
gordonajajar <178007695+gordonajajar@users.noreply.github.com>
2025-08-31 22:32:43 +0800

    Add increment: level-0

---
+++ src/main/java/Bruh.java
@@ -0,0 +1,13 @@
+public class Bruh {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm:\n" +
+                " ____  ____  _   _ _   _ \n" +
+                "| __ )|  _ \\| | | | | | |\n" +
+                "|  _ \\| |_) | | | | |_| |\n" +
+                "| |_) |  _ <| |_| |  _  |\n" +
+                "|____/|_| \\_\\\\___/|_| |_|" +
+                "\n";
+        System.out.println(logo);
+    }
+
+}
