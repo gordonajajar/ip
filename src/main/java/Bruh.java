b7787e5f39171fe16ab7aef6344ecb5004da51cd
gordonajajar <178007695+gordonajajar@users.noreply.github.com>
2025-08-31 22:49:45 +0800

    Add increment: level-1

--- src/main/java/Bruh.java
+++ src/main/java/Bruh.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bruh {
     public static void main(String[] args) {
         String logo = "Hello! I'm:\n" +
@@ -6,8 +8,21 @@ public class Bruh {
                 "|  _ \\| |_) | | | | |_| |\n" +
                 "| |_) |  _ <| |_| |  _  |\n" +
                 "|____/|_| \\_\\\\___/|_| |_|" +
-                "\n";
+                "\n" +
+                "Type something: \n";
         System.out.println(logo);
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            String line = in.nextLine();
+            System.out.println("-----------------------------------------------------------------------------");
+            System.out.println("    You said: " + line);
+            System.out.println("-----------------------------------------------------------------------------");
+            if (line.equals("bye")) {
+                break;
+            }
+        }
     }
+
 
 }
