4a362951449c38fb1642fe61290f5a7b322748af
gordonajajar <178007695+gordonajajar@users.noreply.github.com>
2025-09-17 13:27:30 +0800

    Add saveTasksToDisk function

--- src/main/java/Bruh.java
+++ src/main/java/Bruh.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import exceptions.EmptyDescriptionException;
 import exceptions.WrongTaskNumberException;
@@ -46,6 +49,7 @@ public class Bruh {
     }
 
     private static boolean parseInput(String command, Task[] tasks, String[] input) {
+        boolean isTaskListModified = false;
         try {
             switch (command) {
             case "bye":
@@ -65,31 +69,40 @@ public class Bruh {
                 Todo newToDo = new Todo(input[1]);
                 tasks[Task.getNumberOfTasks() - 1] = newToDo;
                 System.out.println("Added todo: \n" + newToDo + "\n" + "You now have " + Task.getNumberOfTasks() + " tasks.");
+                isTaskListModified = true;
                 break;
             case "deadline":
                 String[] deadlineInput = input[1].split(" /by ", 2);
                 Deadline newDeadline = new Deadline(deadlineInput[0], deadlineInput[1]);
                 tasks[Task.getNumberOfTasks() - 1] = newDeadline;
                 System.out.println("Added deadline: \n" + newDeadline + "\n" + "You now have " + Task.getNumberOfTasks() + " tasks.");
+                isTaskListModified = true;
                 break;
             case "event":
                 String[] eventInput = input[1].split((" /from | /to "), 3);
                 Event newEvent = new Event(eventInput[0], eventInput[1], eventInput[2]);
                 tasks[Task.getNumberOfTasks() - 1] = newEvent;
                 System.out.println("Added event: \n" + newEvent + "\n" + "You now have " + Task.getNumberOfTasks() + " tasks.");
+                isTaskListModified = true;
                 break;
             case "mark":
                 Task taskToMark = getTaskByIndex(tasks, input[1]);
                 taskToMark.setDone();
                 System.out.println("Marked as done: " + taskToMark);
+                isTaskListModified = true;
                 break;
             case "unmark":
                 Task taskToUnmark = getTaskByIndex(tasks, input[1]);
                 taskToUnmark.setUndone();
                 System.out.println("Marked as undone: " + taskToUnmark);
+                isTaskListModified = true;
                 break;
             default:
                 System.out.println("Huh?...");
+            }
+
+            if (isTaskListModified) {
+                saveTasksToDisk(tasks);
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Missing arguments!");
@@ -97,6 +110,8 @@ public class Bruh {
             System.out.println("Your description is empty, try again...");
         } catch (WrongTaskNumberException e) {
             System.out.println("You entered an invalid task number; choose tasks 1 to " + Task.getNumberOfTasks());
+        } catch (IOException e) {
+            System.out.println("Problem saving tasks to file " + e.getMessage());
         }
         return false;
     }
@@ -108,6 +123,27 @@ public class Bruh {
             return tasks[index];
         } catch (NumberFormatException e) {
             throw new WrongTaskNumberException();
+        }
+    }
+
+    private static void saveTasksToDisk(Task[] tasks) throws IOException {
+        String filePath = "./data/tasks.txt";
+        File f = new File(filePath);
+
+        // ensure parent directory exists
+        File parent = f.getParentFile();
+        if (parent != null && !parent.exists() && !parent.mkdirs()) {
+            throw new IOException("Could not create directory " + parent + ", check permission issues?");
+        }
+
+        try (FileWriter fw = new FileWriter(f)) {
+            for (Task task : tasks) {
+                fw.write(task.toSaveString());
+                fw.write(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println("Could not access " + filePath + " for writing :((");
+            throw e;
         }
     }
 }
